a15f408b1e9547cafdd27cf2373ae18d2b8b6104
Charlie Li <lixuan3901@gmail.com>
2025-10-12 03:39:33 +0800

    feat(contact): implement honeypot hidden field and user typing-time validation to block bots

--- src/app/features/contact/contact.component.ts
+++ src/app/features/contact/contact.component.ts
@@ -17,6 +17,7 @@ import { MatCard } from '@angular/material/card';
 import { MatButton, MatButtonModule } from '@angular/material/button';
 import { ContactService } from '../../services/contact.service';
 import { SnackBarService } from '../../services/snackbar.service';
+import { MIN_FILL_TIME } from '../../utils/constants';
 
 @Component({
   selector: 'app-contact',
@@ -39,16 +40,33 @@ export class ContactComponent {
   contactForm: FormGroup;
   isSubmitting = false;
   private snackBarService = inject(SnackBarService);
+  formLoadTime = Date.now();
 
   constructor(private fb: FormBuilder, private contactService: ContactService) {
     this.contactForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(2)]],
       email: ['', [Validators.required, Validators.email]],
       message: ['', [Validators.required, Validators.minLength(10)]],
+      address: [''],
     });
   }
 
   onSubmit() {
+    if (this.isSubmitting) return;
+
+    if (this.contactForm.get('address')?.value) {
+      // 如果地址字段有值，可能是机器人提交，直接返回
+      return;
+    }
+
+    const elapsed = Date.now() - this.formLoadTime;
+    if (elapsed < MIN_FILL_TIME) {
+      this.snackBarService.showInfo(
+        'Form submitted too quickly. Please take your time to fill out the form.'
+      );
+      return;
+    }
+
     if (this.contactForm.invalid) {
       this.contactForm.markAllAsTouched();
       return;
@@ -56,28 +74,30 @@ export class ContactComponent {
 
     this.isSubmitting = true;
 
-    this.contactService.submitContactForm(this.contactForm.value).subscribe({
+    const sendMessage = Object.assign({}, this.contactForm.value);
+    delete sendMessage.address; // 删除隐藏字段
+
+    this.contactService.submitContactForm(sendMessage).subscribe({
       next: () => {
         this.snackBarService.showSuccess(
           'Message sent! I’ll get back to you as soon as possible.'
         );
+        this.resetAllKeys();
       },
       error: (err) => {
         console.error('Error submitting contact form', err);
-        this.isSubmitting = false;
         this.snackBarService.showError(
           'Message not sent. Please check your internet connection or try again later.'
         );
       },
       complete: () => {
-        this.resetAllKeys();
         this.isSubmitting = false;
       },
     });
   }
 
   resetAllKeys() {
-    // this.contactForm.reset();
+    this.contactForm.reset();
     // 🚀 关键修复步骤：在重置后手动清除错误状态
     // 遍历所有控件，确保它们的错误和 'touched' 状态被完全清除
     Object.keys(this.contactForm.controls).forEach((key) => {
---
+++ src/app/utils/constants.ts
@@ -0,0 +1,2 @@
+const MIN_FILL_TIME = 5000;
+export { MIN_FILL_TIME };
